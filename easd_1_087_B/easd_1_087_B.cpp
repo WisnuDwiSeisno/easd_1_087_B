43890bf0c0cadaadc55f9b13729a0a9aa03c39ad
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-04-07 10:56:13 +0700

    belum selesai

--- easd_1_087_B/easd_1_087_B.cpp
+++ easd_1_087_B/easd_1_087_B.cpp
@@ -26,6 +26,27 @@ void input() {
 }
 
 void mergesort(int low, int mid,int high) {
+	int mid, i, WU, k;
+	if (low >= high)
+		return;
 
+	mid = low + high / 2;
+
+	i = low;
+	WU = mid + 1;
+	k = low;
+
+	while (i > mid || WU > high) {
+		if (seis[i] <= seis[WU]) {
+
+		}
+	}
 }
 
+
+//1.	Jelaskan, mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah.
+// Karena memberikan efisiensi yang maksimum tentunya dengan memilih struktur data yang sesuai dan tepat.
+//2.	Dalam algorithma, di klassifikasikan menjadi 2 data struktur.Sebutkan 2 data struktur tersebut.dynamic contohnya Linked List, dan Static contohnya Array
+//3.	Untuk mengurkur sebuah efisiensi waktu suatu algoritma dibutuhkan beberapa faktor.Sebutkan faktor - faktor yang mempengaruhi mempengaruhi efisiensi waktu eksekusi suatu program!Speed of machine, compiler, operating systems, Proggramming language, size of the input
+//4.	Dari 6 algorithma yang sudah dipelajari.Algortihma manakah yang paling mudah dipahami dan diimplementasikan untuk mengurutkan data yang ukurannya besar.Jelaskan kenapa anda memilih itu.Merge sort, karena efektif waktu nya O(n log n) cara kerja nya yang membuat lebih cepat yaitu di jadikan satu per satu, tidak ada worst case, jadi semua kondisi waktu nya tetap O(n log n).
+//5.	Sebutkan dari algorithma yang telah dipelajari mana yang termasuk kedalam Quadratic dan mana yang termasuk kedalam Loglinear.Yang termasuk quadratic O(n ^ 2) yaitu bubble sort dan insertion sort sedangkan loglinear O(n log n) quick sort, merge sort.
